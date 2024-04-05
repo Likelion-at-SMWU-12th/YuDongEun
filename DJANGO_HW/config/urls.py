12551a0f3d1c321f5be90b93e96a369373c6d2b2
uehddms <ehddms3@sookmyung.ac.kr>
2024-04-05 21:44:36 +0900

    취미 변천사와 현재 취미 설명 html 파일 작성 / URL & TEMPLATE & VIEW 사용

--- DJANGO_HW/config/urls.py
+++ DJANGO_HW/config/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from demos.views import week4HW
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('deunu/', week4HW),
 ]
--- DJANGO_HW/demos/views.py
+++ DJANGO_HW/demos/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
 # Create your views here.
+
+def week4HW(request):
+    #return HttpResponse('view 구현해보기!')
+    return render(request, 'week4HW.html')
