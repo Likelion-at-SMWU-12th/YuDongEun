efd39d429923e258d8915124e2932915d732025a
uehddms <ehddms3@sookmyung.ac.kr>
2024-07-16 16:01:46 +0900

    프론트엔드 5주차 과제 완 git add .

--- FEweek05/week5_router/src/App.js
+++ FEweek05/week5_router/src/App.js
@@ -2,6 +2,8 @@ import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Introduce from "./pages/Introduce";
 import Menubar from "./pages/Menubar";
+import Travel from "./pages/Travel";
+import "./App.css";
 
 function App() {
   return (
@@ -9,6 +11,7 @@ function App() {
       <Route path="/" element={<Menubar />}>
         <Route path="home" element={<Home />} />
         <Route path="introduce" element={<Introduce />} />
+        <Route path="travel" element={<Travel />} />
       </Route>
     </Routes>
   );
--- FEweek05/week5_router/src/pages/Menubar.js
+++ FEweek05/week5_router/src/pages/Menubar.js
@@ -1,15 +1,19 @@
 import React from "react";
 import { Link, Outlet } from "react-router-dom";
+import "./Menubar.css";
 
 const Menubar = () => {
   return (
-    <div>
+    <div class="menubar">
       <ul>
         <li>
           <Link to="/home">Home</Link>
         </li>
         <li>
           <Link to="/introduce">Introduce</Link>
+        </li>
+        <li>
+          <Link to="/travel">Travel</Link>
         </li>
       </ul>
       <Outlet />
