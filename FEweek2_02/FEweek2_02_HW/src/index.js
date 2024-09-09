1bc8442e1e5dd845f88ec8a1c29d98178d5eaec0
uehddms <ehddms3@sookmyung.ac.kr>
2024-09-09 21:42:46 +0900

    Provider 컴포넌트 적용

--- FEweek2_02/FEweek2_02_HW/src/index.js
+++ FEweek2_02/FEweek2_02_HW/src/index.js
@@ -2,13 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
-import PartContext from "./contexts/PartContext";
+import PartProvider from "./contexts/PartContext";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <PartContext.Provider value={"프론트엔드"}>
+    <PartProvider>
       <App />
-    </PartContext.Provider>
+    </PartProvider>
   </React.StrictMode>
 );
