b66822f88d4af2c852010cb1e3f962281a5fc5d3
uehddms <ehddms3@sookmyung.ac.kr>
2024-09-09 21:06:09 +0900

    Context 적용 - Provider로 <App/> 감싸주기

--- FEweek2_02/FEweek2_02_HW/src/index.js
+++ FEweek2_02/FEweek2_02_HW/src/index.js
@@ -2,10 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
+import PartContext from "./contexts/PartContext";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <PartContext.Provider value={"프론트엔드"}>
+      <App />
+    </PartContext.Provider>
   </React.StrictMode>
 );
