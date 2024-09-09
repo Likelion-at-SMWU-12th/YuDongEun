f1365acbf35259e5b93916c0d0434bd8f9381b0c
uehddms <ehddms3@sookmyung.ac.kr>
2024-09-09 21:42:08 +0900

    Provider 컴포넌트와 커스텀 훅 생성

--- FEweek2_02/FEweek2_02_HW/src/contexts/PartContext.js
+++ FEweek2_02/FEweek2_02_HW/src/contexts/PartContext.js
@@ -1,5 +1,25 @@
-import { createContext } from "react";
+import { createContext, useContext, useState } from "react";
+import member from "../sookmut";
 
 const PartContext = createContext();
 
-export default PartContext;
+export const PartProvider = ({ children }) => {
+  const [part, setPart] = useState("프론트엔드");
+
+  const memberPart = () => {
+    setPart((part) =>
+      part ? member.filter((member) => member.part === part) : member
+    );
+    console.log("파트 선택: ", part);
+  };
+
+  return (
+    <PartContext.Provider value={{ part, memberPart }}>
+      {children}
+    </PartContext.Provider>
+  );
+};
+
+export default PartProvider;
+
+export const useTheme = () => useContext(PartContext);
