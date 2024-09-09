d05e1d8ee94ea0c5e2a374b34bf770f7af87c9f9
uehddms <ehddms3@sookmyung.ac.kr>
2024-09-09 20:05:48 +0900

    2학기 2주차 세미나 실습

---
+++ FEweek2_02/contextapi/src/App.js
@@ -0,0 +1,21 @@
+import { Route, Routes } from "react-router";
+import ReviewPage from "./pages/ReviewPage";
+import CreatePage from "./pages/CreatePage";
+import ToggleBtn from "./components/ToggleBtn";
+import { ThemeProvider } from "./contexts/ThemeContext";
+
+function App() {
+  return (
+    <>
+      <ThemeProvider value={"light"}>
+        <Routes>
+          <Route path="/" element={<ReviewPage />} />
+          <Route path="/create" element={<CreatePage />} />
+        </Routes>
+        <ToggleBtn />
+      </ThemeProvider>
+    </>
+  );
+}
+
+export default App;
